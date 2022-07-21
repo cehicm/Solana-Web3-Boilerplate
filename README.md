b97579e23c2ea0b1544d3c6c7050f72396a5f163
Nikola Pavlović <nikolap994@gmail.com>
2022-07-22 00:26:21 +0200

    Add boilerplate setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Solana React Quickstart
+
+This is a simple boilerplate for Solana wallet connection through wallet-adapter
+
+
+## Run
+```
+npm run start
+```
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ConnectionProvider, WalletProvider, } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { getLedgerWallet, getPhantomWallet, getSlopeWallet, getSolflareWallet, getSolletExtensionWallet, getSolletWallet, } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import MyWallet from "./MyWallet";
+function App() {
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Mainnet;
+    // You can also provide a custom RPC endpoint
+    const endpoint = React.useMemo(() => clusterApiUrl(network), [network]);
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
+    // Only the wallets you configure here will be compiled into your application
+    const wallets = React.useMemo(() => [
+        getPhantomWallet(),
+        getSlopeWallet(),
+        getSolflareWallet(),
+        getLedgerWallet(),
+        getSolletWallet({ network }),
+        getSolletExtensionWallet({ network }),
+    ], [network]);
+    return (<ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <MyWallet />
+      </WalletProvider>
+    </ConnectionProvider>);
+}
+export default App;
